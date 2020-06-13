564a7b1fcc46c4037e7fbb565784f96867d00ff3
Khattak <akkhattak65@gmail.com>
2020-06-13 20:48:33 +0500

    :fire: Remove Commented Variables

--- public/selectors.js
+++ public/selectors.js
@@ -2,29 +2,10 @@
 export const backspaceBtn = document.querySelector(".buttons-c");
 export const clearBtn = document.querySelector(".buttons-cc");
 export const plusMinusBtn = document.querySelector(".buttons-pm");
-// export const divideBtn = document.querySelector(".buttons-divide");
-// export const multiplyBtn = document.querySelector(".buttons-multiply");
-// export const minusBtn = document.querySelector(".buttons-minus");
-// export const plusBtn = document.querySelector(".buttons-plus");
-// export const modBtn = document.querySelector(".buttons-mod");
 export const equalBtn = document.querySelector(".buttons-equal");
-// export const dotBtn = document.querySelector(".buttons-dot");
-// Numbers
-// export const oneBtn = document.querySelector(".buttons-one");
-// export const twoBtn = document.querySelector(".buttons-two");
-// export const threeBtn = document.querySelector(".buttons-three");
-// export const fourBtn = document.querySelector(".buttons-four");
-// export const fiveBtn = document.querySelector(".buttons-five");
-// export const sixBtn = document.querySelector(".buttons-six");
-// export const sevenBtn = document.querySelector(".buttons-seven");
-// export const eightBtn = document.querySelector(".buttons-eight");
-// export const nineBtn = document.querySelector(".buttons-nine");
-// export const zeroBtn = document.querySelector(".buttons-zero");
 
+// Numbers
 export const btns = {
-  //   backspaceBtn: document.querySelector(".buttons-c"),
-  //   clearBtn: document.querySelector(".buttons-cc"),
-  //   equalBtn: document.querySelector(".buttons-equal"),
   dotBtn: document.querySelector(".buttons-dot"),
   oneBtn: document.querySelector(".buttons-one"),
   twoBtn: document.querySelector(".buttons-two"),
@@ -39,7 +20,6 @@ export const btns = {
 };
 
 export const operations = {
-  //   plusMinusBtn: document.querySelector(".buttons-pm"),
   divideBtn: document.querySelector(".buttons-divide"),
   multiplyBtn: document.querySelector(".buttons-multiply"),
   minusBtn: document.querySelector(".buttons-minus"),
